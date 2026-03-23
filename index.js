5e90f3a8c0eb4f700dcdc10df70d061f4ac143e0
arthur <arthur.oliveira.albuquerque@escola.pr.gov.br>
2026-03-23 09:45:39 -0300

    configuração do servidor express e middlewares da api" Use o código com cuidado

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const produtosRoutes = require('./routes/produtos');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware para processar JSON
+app.use(express.json());
+
+// Uso das rotas
+app.use('/produtos', produtosRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
