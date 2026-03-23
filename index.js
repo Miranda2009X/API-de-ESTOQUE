0d6e7a856875f36cd1ea78a808bfd0590a996323
Miranda2009X <gabriel.afonso.miranda@escola.pr.gov.br>
2026-03-23 09:56:28 -0300

    index.js

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ const produtosRoutes = require('./routes/produtos');
 const app = express();
 const PORT = 3000;
 
-// Middleware para processar JSON
 app.use(express.json());
 
-// Uso das rotas
+
 app.use('/produtos', produtosRoutes);
 
 app.listen(PORT, () => {
